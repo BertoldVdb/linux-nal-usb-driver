9c3532af0234354c6018538a3afe800bd1223421
Bertold Van den Bergh <vandenbergh@bertold.org>
2020-10-20 00:23:26 +0200

    Fix checkpatch style

--- nal.c
+++ nal.c
@@ -67,8 +67,8 @@ static struct usb_serial_driver nal_device = {
 	.port_probe		= nal_port_probe,
 	.port_remove		= nal_port_remove,
 	.dtr_rts		= nal_dtr_rts,
-        .process_read_urb       = nal_process_read_urb,
-        .prepare_write_buffer   = nal_prepare_write_buffer
+	.process_read_urb       = nal_process_read_urb,
+	.prepare_write_buffer   = nal_prepare_write_buffer
 };
 
 static struct usb_serial_driver * const serial_drivers[] = {
@@ -85,8 +85,8 @@ static struct usb_serial_driver * const serial_drivers[] = {
 static int nal_prepare_write_buffer(struct usb_serial_port *port,
 					void *buf, size_t count)
 {
-	struct nal_serial_private *priv = usb_get_serial_port_data(port);	
-	unsigned char *header = (unsigned char*)buf;
+	struct nal_serial_private *priv = usb_get_serial_port_data(port);
+	unsigned char *header = (unsigned char *)buf;
 	unsigned char header_type, cout;
 
 	spin_lock(&priv->lock);
@@ -102,11 +102,11 @@ static int nal_prepare_write_buffer(struct usb_serial_port *port,
 
 	if (!header_type)
 		return 0;
-	else if (header_type == 2){
+	else if (header_type == 2) {
 		header[1] = count;
 		return 64;
 	}
-	
+
 	return cout;
 }
 
@@ -114,7 +114,7 @@ static void nal_process_read_urb(struct urb *urb)
 {
 	struct usb_serial_port *port = urb->context;
 	struct nal_serial_private *priv = usb_get_serial_port_data(port);
-	const unsigned char *buf = (const unsigned char*)urb->transfer_buffer;
+	const unsigned char *buf = (const unsigned char *)urb->transfer_buffer;
 	unsigned char length;
 
 	if (urb->actual_length < 1)
@@ -129,7 +129,7 @@ static void nal_process_read_urb(struct urb *urb)
 	if (priv->header_type != 1)
 		schedule_work(&priv->data_work);
 
-	if (buf[0] == 5 && urb->actual_length >= 2){
+	if (buf[0] == 5 && urb->actual_length >= 2) {
 		if (!priv->header_type)
 			return;
 		else if (priv->header_type == 1)
@@ -147,16 +147,15 @@ static void nal_process_read_urb(struct urb *urb)
 	} else if (buf[0] == 0 && urb->actual_length >= 2) {
 		spin_lock(&priv->lock);
 		priv->control_get = 0x80 | buf[1];
-		if (!priv->header_type && urb->actual_length == 64){
+		if (!priv->header_type && urb->actual_length == 64)
 			priv->header_type = 2;
-		}
 		spin_unlock(&priv->lock);
 
 		wake_up(&priv->control_event);
 	} else if (buf[0] == 1)
 		schedule_work(&priv->control_work);
-	else 
-		dev_info(&priv->dev->dev, "Unsupported input (%u): %02x\n", urb->actual_length, buf[0]);
+	else
+		dev_info(&priv->dev->dev, "Unsupported input (length=%u): %02x\n", urb->actual_length, buf[0]);
 }
 
 static int nal_request(struct nal_serial_private *priv, int type)
@@ -166,7 +165,8 @@ static int nal_request(struct nal_serial_private *priv, int type)
 	mutex_lock(&priv->cmd_mutex);
 
 	/* type==0: Request control lines
-	 * type==1: Request application data */
+	 * type==1: Request application data
+	 */
 	priv->cmd_buf[0] = type?0x04:0x01;
 	priv->cmd_buf[1] = 0xFF;
 
@@ -180,7 +180,7 @@ static int nal_request(struct nal_serial_private *priv, int type)
 
 static void nal_data_work(struct work_struct *work)
 {
-	struct nal_serial_private *priv = 
+	struct nal_serial_private *priv =
 		container_of(work, struct nal_serial_private, data_work);
 
 	nal_request(priv, 1);
@@ -190,7 +190,7 @@ static int nal_tiocmget(struct tty_struct *tty)
 {
 	struct usb_serial_port *port = tty->driver_data;
 	struct nal_serial_private *priv = usb_get_serial_port_data(port);
-        int ret_val, control = 0;
+	int ret_val, control = 0;
 
 	spin_lock(&priv->lock);
 	priv->control_get = 0;
@@ -200,10 +200,10 @@ static int nal_tiocmget(struct tty_struct *tty)
 	if (ret_val)
 		return ret_val;
 
-	while(!control){
+	while (!control) {
 		ret_val = wait_event_interruptible_timeout(priv->control_event,
 				priv->control_get > 0, HZ);
-		if (ret_val == 0) 
+		if (ret_val == 0)
 			ret_val = -ETIMEDOUT;
 		if (ret_val < 0)
 			return ret_val;
@@ -236,18 +236,14 @@ static int nal_send_control(struct nal_serial_private *priv,
 	mutex_lock(&priv->cmd_mutex);
 
 	spin_lock(&priv->lock);
-	if (set & TIOCM_RTS) {
+	if (set & TIOCM_RTS)
 		priv->control_put |= CONTROL_RTS;
-	}
-	if (set & TIOCM_DTR) {
+	if (set & TIOCM_DTR)
 		priv->control_put |= CONTROL_DTR;
-	}
-	if (clear & TIOCM_RTS) {
+	if (clear & TIOCM_RTS)
 		priv->control_put &= ~CONTROL_RTS;
-	}
-	if (clear & TIOCM_DTR) {
+	if (clear & TIOCM_DTR)
 		priv->control_put &= ~CONTROL_DTR;
-	}
 
 	control = priv->control_put;
 	spin_unlock(&priv->lock);
@@ -284,7 +280,7 @@ static void nal_dtr_rts(struct usb_serial_port *port, int enable)
 
 static void nal_control_work(struct work_struct *work)
 {
-	struct nal_serial_private *priv = 
+	struct nal_serial_private *priv =
 		container_of(work, struct nal_serial_private, control_work);
 
 	nal_send_control(priv, 0, 0);
@@ -314,7 +310,7 @@ static int nal_port_probe(struct usb_serial_port *serial)
 
 	INIT_WORK(&priv->control_work, nal_control_work);
 	INIT_WORK(&priv->data_work, nal_data_work);
-	
+
 	/* Used for header autodetect */
 	ret_val = nal_request(priv, 0);
 	if (ret_val < 0)
